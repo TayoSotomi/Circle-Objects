49cee55989a2e33dd70d4ab2c7e01250f031acf5
Tee <sotomi.tayo@gmail.com>
2023-01-24 01:15:30 -0600

    First Submit

---
+++ Circle Objects/Circle.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_Objects
+{
+    public class Circle
+    {
+        //properties
+        public double Radius {get; set;}
+        public double Area { get; set;}
+        public double Circumference { get; set; }
+
+
+
+        //Methods
+        public static int count = 0;
+
+        public Circle (double radius)
+        {
+            Radius = radius;
+            count++;
+
+        }
+
+        public double CalculateCircumference()
+        {       
+            double Circumference = 2 * Math.PI * Radius;
+            return Circumference;
+            
+
+        }
+
+        public string CalculateFormatedCircumference()
+        {
+            double Value = CalculateCircumference();
+            return $"Circumference:  {FormatNumber(Value)}";
+            
+
+        }
+
+        public double CalculateArea()
+        {
+            Area = Math.PI * (Radius * Radius);
+
+            return Area;
+            
+
+        }
+
+        public string CalculateFormattedArea()
+        {
+            double Value = CalculateArea();
+            return $"Area:  {FormatNumber(Value)}";
+            
+
+        }
+
+
+        private string FormatNumber(double Value)
+        {
+            return (Math.Round(Value, 2)).ToString();
+            
+
+        }
+
+        public static int getCount()
+        {
+            return count;
+        }
+
+
+    }
+}
---
+++ Circle Objects/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using Circle_Objects;
+using System.Security.Cryptography.X509Certificates;
+
+bool ToContinue = true;
+double radius;
+
+Console.WriteLine("Welcome to the Circle Tester");
+Console.WriteLine();
+
+while (ToContinue)
+{
+
+    Console.Write("Please enter radius: ");
+
+    while( double.TryParse(Console.ReadLine(),out radius) ==false)
+    {
+        Console.WriteLine("invalid input. Please enter radius");
+    }
+    if (true)
+    {
+        Circle myCircle = new Circle(radius);
+        Console.WriteLine(myCircle.CalculateFormattedArea());
+        Console.WriteLine(myCircle.CalculateFormatedCircumference());
+    }
+
+    Console.WriteLine("Would you like to continue? y/n ");
+    string input = Console.ReadLine();
+
+
+    if(input == "y")
+    {
+        ToContinue = true;
+    }
+    else if(input == "n")
+    {
+        ToContinue = false;
+        Console.WriteLine($"Goodbye. You created {Circle.getCount()} Circle object(s).");
+    }
+    else
+    {
+        Console.WriteLine("Please enter valid value");
+    }
+}
+
+
+
+
+
+
+
